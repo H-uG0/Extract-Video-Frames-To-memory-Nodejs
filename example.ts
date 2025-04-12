e3a8f2ca79d18095b79ef8282accee834bf81207
Hugo DESSAUD <hugo.dessaud@etu.univ-amu.fr>
2025-04-12 23:35:14 +0200

    added test video

--- example.ts
+++ example.ts
@@ -1,5 +1,5 @@
 import readVideo from "./src/index.ts";
 
-const videoPath = "./video.mp4";
+const videoPath = "./public/video.mp4";
 // return Frames in array of buffers [buffer<> , buffer<> ,.....]
 const frames = readVideo(videoPath).then((data) => console.log(data.length));
