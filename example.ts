44f4be9d57ce6e5b32d60bfa8d850fe44958cc98
Hugo DESSAUD <hugo.dessaud@etu.univ-amu.fr>
2025-04-13 00:00:57 +0200

    deleted useless files

--- example.ts
+++
@@ -1,5 +0,0 @@
-import readVideo from "./src/index.ts";
-
-const videoPath = "./public/video.mp4";
-// return Frames in array of buffers [buffer<> , buffer<> ,.....]
-const frames = readVideo(videoPath).then((data) => console.log(data.length));
